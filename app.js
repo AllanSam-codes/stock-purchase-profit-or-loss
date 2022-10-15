90e237c9c7953eed7477c716b79b4768c1f2c011
AllanSam <93815162+AllanSam-codes@users.noreply.github.com>
2022-10-15 23:58:10 +0530

    Updated input fields and fetched the same user data on js file

---
+++ app.js
@@ -0,0 +1,6 @@
+const inputStock = document.querySelectorAll(".input-stock");
+const stockBtn = document.querySelector("#stock-btn")
+const outputBox = document.querySelector("#output-box")
+
+
+console.log(inputStock, stockBtn, outputBox);
